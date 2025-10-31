4526a440b2779714185de3be1f5943fff8508f81
Sahil <sahil2215ali@gmail.com>
2025-11-01 03:59:32 +0530

    chore:updated ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -33,8 +33,8 @@ const Header = () => {
           {/* Logo */}
           <Link to="/" className="flex items-center gap-2 hover:opacity-80 transition-opacity">
             <div className="relative">
-              <Warehouse className="h-8 w-8 text-primary" />
-              <Leaf className="h-4 w-4 text-secondary absolute -bottom-1 -right-1" />
+              {/* <Warehouse className="h-8 w-8 text-primary" />
+              <Leaf className="h-4 w-4 text-secondary absolute -bottom-1 -right-1" /> */}
             </div>
             <span className="text-xl font-bold bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent">
               Ceres Protocol
